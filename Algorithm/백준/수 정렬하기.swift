2ef28122a8961d2c3134d3a149cc45f8f17c6c70
jeon-soyeong <jsu3417@naver.com>
2021-04-19 18:19:40 +0900

    수 정렬하기 - 퀵 정렬, 계수 정렬

--- Algorithm/백준/수 정렬하기.swift
+++ Algorithm/백준/수 정렬하기.swift
@@ -60,3 +60,41 @@ func insertionSorting(){
     }
 }
 
+//퀵정렬
+func quickSorting(_ array: [Int]) -> [Int] {
+    guard array.count > 1 else {
+        return array
+    }
+
+    let pivot = array[array.count/2]
+    let less = array.filter { $0 < pivot }
+    let equal = array.filter { $0 == pivot }
+    let greater = array.filter { $0 > pivot }
+
+    return quickSorting(less) + equal + quickSorting(greater)
+  }
+
+
+//계수 정렬
+func countSorting(_ array:[Int]) -> [Int] {
+    let maxElement = array.max() ?? 0
+    
+    var countArray = [Int](repeating: 0, count: Int(maxElement + 1))
+    for element in array {
+        countArray[element] += 1
+    }
+    
+    for index in 1 ..< countArray.count {
+        let sum = countArray[index] + countArray[index - 1]
+        countArray[index] = sum
+    }
+    
+    var sortedArray = [Int](repeating: 0, count: array.count)
+    for index in stride(from: array.count - 1, through: 0, by: -1) {
+        let element = array[index]
+        countArray[element] -= 1
+        sortedArray[countArray[element]] = element
+    }
+    
+    return sortedArray
+}
