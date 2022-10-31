f0e580cae326458093ce3ec65c7c82e75e72f761
전소영 <jsu3417@gmail.com>
2022-10-31 21:40:48 +0900

    캐시

--- Algorithm/프로그래머스/캐시.swift
+++ Algorithm/프로그래머스/캐시.swift
@@ -7,32 +7,32 @@
 
 import Foundation
 
-func solution(_ cacheSize: Int, _ cities: [String]) -> Int {
-    var caches: [String] = []
-    var time: Int = 0
-    let cities = cities.map { $0.lowercased() }
-    
+func cache(_ cacheSize: Int, _ cities: [String]) -> Int {
+    var cache: [String] = []
+    var time = 0
+
     if cacheSize == 0 {
-        return cities.count * 5
+        time = cities.count * 5
+        return time
     }
-    
-    for i in 0..<cities.count {
-        if !caches.contains(cities[i]) {
-            if caches.count < cacheSize {
-                caches.append(cities[i])
-            } else {
-                caches.removeFirst()
-                caches.append(cities[i])
+
+    for city in cities {
+        let lowerCity = city.lowercased()
+
+        if cache.contains(lowerCity) {
+            guard let cityIndex = cache.firstIndex(of: lowerCity) else {
+                return 0
+            }
+            cache.remove(at: cityIndex)
+            time += 1
+        } else {
+            if cacheSize == cache.count {
+                cache.removeLast()
             }
             time += 5
-        } else {
-            if let cityIndex = caches.firstIndex(of: cities[i]) {
-                let temp = caches.remove(at: cityIndex)
-                caches.append(temp)
-                time += 1
-            }
         }
+        cache.insert(lowerCity, at: 0)
     }
-    
+
     return time
 }
