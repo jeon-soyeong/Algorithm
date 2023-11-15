9aabc8f38ac75a06b0c65d3f38ece8e93fd7efa3
전소영 <jsu3417@gmail.com>
2023-11-15 18:05:58 +0900

    메뉴 리뉴얼

--- Algorithm/프로그래머스/메뉴 리뉴얼.swift
+++ Algorithm/프로그래머스/메뉴 리뉴얼.swift
@@ -6,49 +6,42 @@
 //
 
 import Foundation
+//orders                                             course     result
+//["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]    [2,3,4]    ["AC", "ACDE", "BCFG", "CDE"]
+func solution724111(_ orders: [String], _ course: [Int]) -> [String] {
+    var dictionary: [String: Int] = [:]
+    var result: [String] = []
 
-func generateCombination(order: [Character], combinations: inout [String], combinationString: inout String, stringSize: Int, index: Int) {
-    if combinationString.count == stringSize {
-        combinations.append(combinationString)
-        return
-    }
-    
-    for index in index..<order.count {
-        combinationString.append(order[index])
-        generateCombination(order: order, combinations: &combinations, combinationString: &combinationString, stringSize: stringSize, index: index + 1)
-        combinationString.removeLast()
-    }
-}
+    func combination(_ target: [Character], _ targetCount: Int, _ index: Int, _ temp: String) {
+        if targetCount == temp.count {
+            if dictionary[temp] == nil {
+                dictionary[temp] = 0
+            }
+            dictionary[temp]! += 1
+        }
 
-func solution(_ orders: [String], _ course: [Int]) -> [String] {
-    let sortedOrders = orders.map { $0.sorted() }
-    var answer: Set<String> = []
-    var orderCombinations: [String] = []
-    
-    for order in sortedOrders {
-        for i in course {
-            var combinations: [String] = []
-            var initialString: String = ""
-            generateCombination(order: Array(order), combinations: &combinations, combinationString: &initialString, stringSize: i, index: 0)
-            orderCombinations += combinations
+        for i in index..<target.count {
+            combination(target, targetCount, i + 1, temp + [target[i]])
         }
     }
-    
-    for number in course {
-        var combinationCounts: [String: Int] = [:]
-        let courseCombination = orderCombinations.filter { $0.count == number }
-        
-        for combination in courseCombination {
-            combinationCounts[combination] = courseCombination.filter { $0 == combination }.count
-        }
-        
-        let maxCount = combinationCounts.values.max()
-        if maxCount == 1 { continue }
-        
-        combinationCounts.keys.filter { combinationCounts[$0] == maxCount }.forEach {
-            answer.insert($0)
+
+    for c in course {
+        for order in orders {
+            combination(Array(order).sorted(by: <), c, 0, "")
         }
     }
+
+    for c in course {
+        let sortedDictionary = dictionary.filter { $0.key.count == c && $0.value >= 2 }.sorted { $0.value > $1.value }
+        
+        if sortedDictionary.isEmpty {
+            continue
+        }
+        let maxValue = sortedDictionary.first!.value
+        
+        result.append(contentsOf: sortedDictionary.filter { $0.value == maxValue }.map { $0.key })
+    }
     
-    return Array(answer.sorted())
+    return result.sorted(by: <)
+
 }
